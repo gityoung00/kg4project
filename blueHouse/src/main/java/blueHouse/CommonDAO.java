28c82412b5fe157fdb5b79a1745a02e64bf61686
gayeon <gayeun33@gmail.com>
2022-06-03 21:01:43 +0900

    planning

--- blueHouse/src/main/java/blueHouse/CommonDAO.java
+++ blueHouse/src/main/java/blueHouse/CommonDAO.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class CommonDAO {
+	
 	private static String url ="jdbc:oracle:thin:@bluehouse_high?TNS_ADMIN=/Users/gayeonkim/Wallet_bluehouse";
 	private static String user = "admin";
 	private static String pwd = "Bluehouse1234";
@@ -21,5 +22,25 @@ public class CommonDAO {
 	
 	return conn;
 	}
+	
+	public static void main(String[] args) {
+		Connection con = null;
+		
+		String url = "jdbc:oracle:thin:@DBlocal_high?TNS_ADMIN=/Users/gayeonkim/Wallet_DBlocal";
+		String user = "admin";
+		String pwd = "KGproject1234!";
 
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			con = DriverManager.getConnection(url, user, pwd);
+			System.out.println("드라이브 로딩 성공");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+	
+	}
 }
