1a479570d45c710be5434f2d1ada533d9c396009
gayeon <gayeun33@gmail.com>
2022-06-07 21:37:23 +0900

    login and register page (front and js)

--- blueHouse/src/main/webapp/member/login.jsp
+++ blueHouse/src/main/webapp/member/login.jsp
@@ -4,9 +4,125 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<title>청와대 로그인</title>
+<style>
+	body {
+		margin: 0px;
+	}
+	#member-page {
+		height: 100vh;
+    	background: url(../img/member/memberLayer.png) no-repeat center;
+    	background-size: cover;
+		
+	}
+	
+	#member-block {
+		position: relative;
+		top:180px;
+		margin: auto;
+		background-color: #fff;
+		width: 730px;
+		height: 570px;
+		border-radius: 9px;
+		font-align: center;
+		box-shadow: 0 24px 57px -2px rgba(0, 0, 0, 0.3);
+	}
+	
+	.member-title {
+		font-family: "넥슨Lv1고딕 Low OTF";
+		font-size: 35px;
+		font-weight: bold;
+		position: relative;
+		left: 190px;
+		top: 80px;
+	}
+	
+	.login-input {
+		position: relative;
+		top: 130px;
+		margin: auto;
+		border: 0px;
+		align: center;
+	}
+	
+	.login-input tr {
+		height: 100px;
+		text-align: center;
+	}
+	
+	.login-id, .login-pw {
+		outline: 0;
+		padding: 5px;
+		border: 0;
+		border-bottom: 2px solid #19a0b5;
+		width: 500px;
+		font-size: 20px;
+	}
+	
+	.login-register, .login-submit {
+		border: 0;
+		color: #ffffff;
+		font-weight: bold;
+		font-size: 23px;
+		box-shadow: 3px 7px 20px 2px rgb(0 0 0 / 30%);
+		background-color: #19a0b5;
+		width: 230px;
+		height: 60px;
+	}
+	
+	.login-register {
+		background-color: #058CFF;
+	}
+	
+	.member-find {
+		font-family: "넥슨Lv1고딕 Low OTF";
+		font-size: 20px;
+		color: #B0B0B0;
+		position: relative;
+		left: 250px;
+		top: 140px;
+	}
+
+
+
+</style>
 </head>
 <body>
+<div id="member-page">
+	<div id="member-block">
+		<div class="member-title">대한민국 청와대 로그인</div>
+		<form action="loginSvc.jsp" method="post" id="f">
+	 		<table class="login-input">
+	 			<tr>
+		 			<td colspan="2">
+		 				<input type="text" name="id" class="login-id" placeholder="아이디 입력">
+		 			</td>
+	 			</tr>
+	 			<tr>
+		 			<td colspan="2">
+		 				<input type="password" name="pw" class="login-pw" placeholder="비밀번호 입력">
+		 			</td>
+	 			</tr>
+	 			
+	 			<tr>
+		 			<td><input type="submit" class="login-submit" value="로그인"></td>
+		 			<td><input type="button" class="login-register" value="회원가입" onclick="location.href='register.jsp'"></td>
+	 			</tr>
+	 		</table>
+	 	</form>
+	 	
+	 	<div class="member-find">
+	 		<a href="idFind.jsp">아이디 찾기</a>  |  <a href="pwFind">비밀번호 찾기</a>
+	 	</div>
+	
+	
+	
+	
+	
+	
+	</div>
+</div>
+
 
 </body>
 </html>
---
+++ blueHouse/src/main/webapp/member/register.js
@@ -0,0 +1,169 @@
+function tab(field, event) {
+	    if (event.which == 13 || event.keyCode == 13) {
+		        for (i = 0; i < field.form.elements.length; i++) {
+			            if (field.form.elements[i].tabIndex == field.tabIndex + 1) {
+				                field.form.elements[i].focus();
+				                if (field.form.elements[i].type == "text") {
+					                    field.form.elements[i].select();
+					                    break;                
+								}
+			            }
+		        }
+        		return false;
+		    }
+		    return true;
+		}
+
+
+function idCheck() {
+	var id = document.getElementById("id").value;
+	var msg = document.getElementById('idAlert');
+	msg.style.color = "#058CFF"
+	msg.style.fontWeight = "bold";
+	msg.style.fontSize = "11px";
+	msg.style.position = "relative";
+	msg.style.left = "10px";
+	msg.innerHTML = " ";
+	
+	
+	console.log(id);
+	
+	if (id == "") {
+		msg.style.color = "red";
+		msg.innerHTML = "필수 정보입니다.";
+		return;
+	}
+
+	var isId = /^[a-z0-9][a-z0-9_-]{4,19}$/;
+	if ( (isId.test(id)) == false ) {
+		msg.innerHTML = "5~20자의 영문 소문자, 숫자만 사용 가능합니다.";
+		return;
+	}
+
+}
+function pwCheck() {
+	var pw = document.getElementById("pw").value;
+	var msg2 = document.getElementById('pwAlert');
+	msg2.style.color = "#058CFF"
+	msg2.style.fontWeight = "bold";
+	msg2.style.fontSize = "11px";
+	msg2.style.position = "relative";
+	msg2.style.left = "10px";
+	
+	console.log(pw);
+
+	if (pw == "") {
+		msg.style.color = "red";
+		msg2.innerHTML = "필수 정보입니다.";
+		return;
+	}
+
+	var isPW = /^[A-Za-z0-9`\-=\\\[\];',\./~!@#\$%\^&\*\(\)_\+|\{\}:"<>\?]{8,20}$/;
+	if (!isPW.test(pw)) {
+		msg2.innerHTML = "8~20자 영문 대 소문자, 숫자, 특수문자를 사용하세요.";
+		return;
+	}
+
+	msg2.innerHTML = "";
+}
+
+function confirmCheck() {
+	var pw = document.getElementById("pw").value;
+	var confirmPw = document.getElementById("pwChk").value;
+	var msg = document.getElementById('pwChkAlert');
+	msg.style.color = "red";
+	msg.style.fontWeight = "bold";
+	msg.style.fontSize = "11px";
+	msg.style.position = "relative";
+	msg.style.left = "10px";
+	
+	console.log(pw);
+	console.log(confirmPw);
+	
+	if (confirmPw == "") {
+		msg.innerHTML = "필수 정보입니다.";
+		return;
+	}
+
+	if (pw != confirmPw) {
+		msg.innerHTML = "비밀번호가 일치하지 않습니다.";
+		return;
+	}
+	msg.innerHTML = "";
+}
+
+function nameCheck() {
+	var name = document.getElementById("name").value;
+	var msg = document.getElementById('nameAlert');
+	msg.style.color = "red";
+	msg.style.fontWeight = "bold";
+	msg.style.fontSize = "11px";
+	msg.style.position = "relative";
+	msg.style.left = "10px";
+	
+	console.log(name);
+	
+	if (name == "") {
+		msg.style.color = "red";
+		msg.innerHTML = "필수 정보입니다.";
+		return;
+	}
+
+	var nonchar = /[^가-힣a-zA-Z0-9]/gi;
+	if (nonchar.test(name)) {
+		msg.innerHTML = "한글과 영문 대 소문자를 사용하세요. (특수기호, 공백 사용 불가)";
+		return;
+	}
+	msg.innerHTML = "";
+}
+
+function telCheck() {
+	var tel = document.getElementById("tel").value;
+	var msg = document.getElementById('telAlert');
+	msg.style.color = "red";
+	msg.style.fontWeight = "bold";
+	msg.style.fontSize = "11px";
+	msg.style.position = "relative";
+	msg.style.left = "10px";
+	
+	console.log(tel);
+	
+	if (tel == "") {
+		msg.style.color = "red";
+		msg.innerHTML = "필수 정보입니다.";
+		return;
+	}
+
+	var isTel = /^01([0|1|6|7|8|9])-?([0-9]{3,4})-?([0-9]{4})$/;
+	if (!isTel.test(tel)) {
+		msg.innerHTML = "숫자만 입력할 수 있습니다";
+		return;
+	}
+	msg.innerHTML = "";
+}
+
+function emailCheck() {
+	var email = document.getElementById("email").value;
+	var msg = document.getElementById('emailAlert');
+	msg.style.color = "red";
+	msg.style.fontWeight = "bold";
+	msg.style.fontSize = "11px";
+	msg.style.position = "relative";
+	msg.style.left = "10px";
+	
+	console.log(email);
+	
+	if (email == "") {
+		msg.style.color = "red";
+		msg.innerHTML = "필수 정보입니다.";
+		return;
+	}
+
+	var isEmail = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
+	if (!isEmail.test(email)) {
+		msg.innerHTML = "유효한 이메일 주소가 아닙니다.";
+		return;
+	}
+	msg.innerHTML = "";
+}
+
