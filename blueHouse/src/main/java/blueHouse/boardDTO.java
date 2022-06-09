f74ab74174ea4ac1bc0ea00dd85e1c915ae49996
gayeon <gayeun33@gmail.com>
2022-06-09 15:42:40 +0900

    boardDTO

---
+++ blueHouse/src/main/java/blueHouse/boardDTO.java
@@ -0,0 +1,57 @@
+package blueHouse;
+
+public class boardDTO {
+	
+	private int num;
+	private String title;
+	private String content;
+	private String name;
+	private String id;
+	private int write_date;
+	private String file_name;
+	
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public int getWrite_date() {
+		return write_date;
+	}
+	public void setWrite_date(int write_date) {
+		this.write_date = write_date;
+	}
+	public String getFile_name() {
+		return file_name;
+	}
+	public void setFile_name(String file_name) {
+		this.file_name = file_name;
+	}
+	
+
+}
